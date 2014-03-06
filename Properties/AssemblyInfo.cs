0f5d551cf643dfb195fc40ce713e412476cf2e0a
Craig Lebowitz <lebowitz@ngpvan.com>
2014-03-06 13:12:31 -0500

    initial

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAndLoadTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NGP VAN")]
+[assembly: AssemblyProduct("WebAndLoadTestProject2")]
+[assembly: AssemblyCopyright("Copyright © NGP VAN 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("708c9d26-9356-4176-b038-2a5b0ab4d3f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QueueTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading;
+using System.Transactions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nv.PriorityQueue
+{
+    interface IPriorityQueue {
+        public IEnumerable<object o> Dequeue();
+        public long Enqueue(object o);    
+    }
+
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        public void Enqueue()
+        {
+            using (var c = new QueueContext())
+            {
+                c.Enqueue((byte)StaticRandom.Next(1, 128), (short)StaticRandom.Next(1, 8), Guid.NewGuid().ToString());
+            }
+        }
+
+        [TestMethod]
+        public void Dequeue()
+        {
+            using (var c = new QueueContext())
+            {
+                using (var s = new TransactionScope(TransactionScopeOption.RequiresNew,
+                        new TransactionOptions() {IsolationLevel = IsolationLevel.ReadCommitted}))
+                {                    
+                    var workBatch = c.Dequeue();
+
+                    foreach (var w in workBatch)
+                    {
+                        // do work
+                        if (w != null)
+                        {
+                            c.AcknowledgeCompletion(w.Id);
+                        }
+                    }
+
+                    s.Complete();
+                }
+            }
+
+        }
+    }
+}
